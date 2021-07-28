7704347fa42fa0994f2f87b97134fe9e33f2d0dc
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2021-07-28 13:36:44 +0000

    create a function to display and shuffle questions at random

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
 const startButton = document.getElementById('start-btn');
-const questionContainer = document.getElementById('question-container');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
 
 function startGame() {
     console.log('Started');
     startButton.classList.add('hide');
-    questionContainer.classList.remove('hide');
-    nextQuestion()
+    shuffledQuestions = questions.sort(() => Math.random - 0.5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
 }
 
-function nextQuestion() {
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question;
 }
 
 function selectAnswer() {
@@ -19,5 +29,31 @@ function selectAnswer() {
 }
 
 const questions = [
-    
+    {
+        question: 'Who said, “On October 3rd, he asked me what day it was."?',
+        answers: [
+            {text: 'Cady', correct: true},
+            {text: 'Karen', correct: false},
+            {text: 'Aaron', correct: false},
+            {text: 'Regina', correct: false},
+        ]
+    }
+    , {
+        question: 'Who said, “Get in, loser. We’re going shopping!”?',
+        answers: [
+            {text: 'Mrs. George', correct: false},
+            {text: 'Regina', correct: true},
+            {text: 'Janis', correct: false},
+            {text: "Cady's Mom", correct: false},
+        ]
+    }
+    , {
+        question: 'Who said. "I am sorry that people are so jealous of me. But I cannot help it that I am popular."?',
+        answers: [
+            {text: 'Karen', correct: false},
+            {text: 'Damian', correct: false},
+            {text: 'Kevin', correct: false},
+            {text: 'Gretchen', correct: true},
+        ]
+    }
 ]
